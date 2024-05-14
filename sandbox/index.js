4639ed827aa1c060a4196d66546354aaafaf1609
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-05-14 19:48:41 +0300

    Dynamic import - done

--- sandbox/index.js
+++ sandbox/index.js
@@ -2,21 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 // __dirname - константа ноди, яка зберігає абсолютну адресу поточної папки
-// __filename - константа ноди, яка зберігає абсолютну адресу поточного файлу
+// __filename - контанта ноди, яка зберігає абсолютну адресу поточного файлу
 
 const currentFileName = path.basename(__filename); // index.js
 
 const db = {};
 
-const filesNames = fs.readdirSync(__dirname);
-const filteredArray = filesNames.filter(
-    (currentFile) => /.js$/.test(currentFile) && currentFile !== currentFile
-);
+fs.readdirSync(__dirname)
+    .filter(
+        (currentFile) =>
+            /.js$/.test(currentFile) && currentFile !== currentFileName
+    )
+    .forEach((currentFile) => {
+        const absPathToFile = path.resolve(__dirname, currentFile);
 
-filteredArray.forEach(currentFile => {
-    const absPathToFile = path.resolve(__dirname, currentFile);
+        const Model = require(absPathToFile);
+        Model._client = client;
+        db[Model.name] = Model;
+    });
     
-    const Model = require(absPathToFile);
-    Model._client = client;
-    db[Model._tableName] = Model;
-});
