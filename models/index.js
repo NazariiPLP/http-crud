3577eda7a82c55254a9dfc310e4f7bc7a6aaeb9b
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-05-14 19:40:07 +0300

    App + Model + Sandbox

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+const config = require('../configs/postgres.json');
+const Thing = require('./Thing');
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env]; // dbConfig = config.development
+
+const client = new Client(dbConfig);
+
+client.connect();
+
+process.on('beforeExit', () => {
+    client.end();
+});
+
+module.exports = {
+    client,
+    Thing
+};
